d11066bb21a9c480b0ec6872c034538c4f78bace
Luca Trinca <lucatrinca.uk@gmail.com>
2025-05-12 20:02:59 +0000

    That is not a meeple. I mean a board game meeple

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -8,15 +8,12 @@ export function Header() {
           <svg
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
+            viewBox="0 0 512 512" // Adjusted viewBox for meeple SVG
             fill="currentColor"
             className="h-8 w-8"
           >
-            <path 
-              fillRule="evenodd" 
-              d="M10 2a2 2 0 100 4 2 2 0 000-4zM3.478 6.129A.75.75 0 003 6.75v5.5a.75.75 0 00.75.75h12.5a.75.75 0 00.75-.75v-5.5a.75.75 0 00-.478-.68l-2.577-1.196a.75.75 0 00-.944.24l-1.722 3.208a.75.75 0 01-1.362 0L7.72 5.203a.75.75 0 00-.944-.24L3.478 6.13zM3 13.5v2A1.5 1.5 0 004.5 17h2A1.5 1.5 0 008 15.5v-2h4v2A1.5 1.5 0 0013.5 17h2a1.5 1.5 0 001.5-1.5v-2H3z" 
-              clipRule="evenodd" 
-            />
+            {/* Meeple SVG Path - Sourced from a generic meeple icon */}
+            <path d="M256 0C181.3 0 120.4 60.91 120.4 135.6C120.4 159.1 126.7 181.9 137.7 201.7L37.22 273.7C14.03 290.5 0 318.8 0 349.1C0 403.1 41.69 448 93.41 448H159.6V512H352.4V448H418.6C470.3 448 512 403.1 512 349.1C512 318.8 497.1 290.5 474.8 273.7L374.3 201.7C385.3 181.9 391.6 159.1 391.6 135.6C391.6 60.91 330.7 0 256 0Z"/>
           </svg>
           <h1 className="text-xl font-bold tracking-tight sm:text-2xl">BoardGame Ranker</h1>
         </Link>
